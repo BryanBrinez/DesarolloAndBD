f79d91ef443404b1e99c6ce84de58934c838c8ff
BryanBrinez <94941570+BryanBrinez@users.noreply.github.com>
2022-11-30 20:44:32 -0500

    hola

---
+++ api/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require("cors");
+const { Pool } = require('pg');
+const { Client } = require('pg');
+
+const port = 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.post("/beneficiario", (req, res) => {
+  const benData = req.body;
+
+  //console.log(benData.id_1);
+  inserBen(benData.paren, benData.id_1, benData.id_2);
+
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+
+async function inserBen(ben, id1, id2) {
+  try {
+    const client = new Client({
+      user: 'postgres',
+      host: 'localhost',
+      password: '123456789',
+      database: 'healthsoft'
+    });
+    await client.connect();
+
+    const queryToInsert = "insert into beneficiario values ('" + ben + "','" + id1 + "','" + id2 + "')";
+
+
+    const res = await client.query(queryToInsert);
+
+    console.log(res.rows); // Hello world!
+    await client.end();
+  }
+  catch (error) {
+    console.log(error);
+
+  }
+}
